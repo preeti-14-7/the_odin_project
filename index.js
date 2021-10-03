da400c30585ed822882aef013dcc918c0c3f8f58
preeti-14-7 <shaiyna.bdr@gmail.com>
2021-10-03 21:22:48 +0530

    captilize and last letter code of js

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function captilize(str){
+   //return str.charAt(0).toUpperCase()+ str.slice(1);
+   //return s.charAt(0).toUpperCase() + s.slice(1)
+   let n = str.length;
+   return str.charAt(n-1);
+}
